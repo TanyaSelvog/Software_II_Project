371f54b2e8c0f1d4e054edb8a4335ec3e2d6af8e
Tanya Selvog <tanyaselvog@gmail.com>
2021-11-30 10:56:59 -0800

    added loginController and started added fxids for every window done

--- src/controller/mainController.java
+++ src/controller/mainController.java
@@ -1,4 +1,14 @@
 package controller;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class mainController {
+    public Button loginBtn;
+    public Label usernameLbl;
+    public Label passwordLbl;
+    public TextField usernameTF;
+    public TextField passwordTF;
+    public Label zoneIdLbl;
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -8,7 +8,15 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/loginWindow.fxml"));
+        primaryStage.setTitle("Welcome to Scheduler!");
+        primaryStage.setScene(new Scene(root, 600, 500));
+        primaryStage.show();
 
+    }
+
+    public static void main(String [] args){
+        launch(args);
     }
 }
