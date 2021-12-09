9bcf3b513a815856f55c6ad6e176eaf906486132
Tanya Selvog <tanyaselvog@gmail.com>
2021-12-09 13:10:47 -0800

    started method in newApptController for user input

--- src/controller/newApptController.java
+++ src/controller/newApptController.java
@@ -2,14 +2,20 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import model.Appointments;
 
-public class newApptController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class newApptController implements Initializable {
     public TextField apptID;
     public TextField titleTF;
     public TextField descTF;
@@ -21,6 +27,32 @@ public class newApptController {
     public TextField startTF;
     public Button saveBtn;
     public Button cancelBtn;
+    /**
+     * This method initializes the controller.
+     *
+     * @param url            Used to resolve relative paths for the root object, or null if the location is not known.
+     * @param resourceBundle Used to localize the root object, or null if the root object was not localized.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+
+    /** TODO started on method to get data from users; need to make System.out.println/String methods for testing
+     */
+
+    public Appointments newAppointment() {
+        try {
+            String apptDescription = descTF.getText();
+
+        } catch (Exception displayE) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, ("Data is missing or contains invalid values."));
+            alert.showAndWait();
+
+        }
+            return null;
+        }
 
     public void onSave(ActionEvent actionEvent) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/view/appointmentsView.fxml"));
@@ -29,6 +61,9 @@ public class newApptController {
         Scene scene = new Scene(root, 1000, 600);
         stage.setScene(scene);
         stage.show();
+
+        Appointments appt = newAppointment();
+
     }
 
 
