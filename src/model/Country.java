2ba9a2c5a6210fff37715af5961423cf84d40eb6
Tanya Selvog <tanyaselvog@gmail.com>
2021-12-27 09:37:30 -0800

    created CountryDB and Country classes

---
+++ src/model/Country.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Country {
+
+    private String countryName;
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName){
+        this.countryName = countryName;
+    }
+}
