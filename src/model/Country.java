03cfa870313cce3e34b413753704f3d8b5d7ceb1
Tanya Selvog <tanyaselvog@gmail.com>
2021-12-28 19:02:38 -0800

    made Country class constructor method

--- src/model/Country.java
+++ src/model/Country.java
@@ -3,12 +3,32 @@ package model;
 public class Country {
 
     private String countryName;
+    private int countryID;
+    /*
+    Constructor
+
+     */
+    public Country (String countryName, int countryID){
+        this.countryName = countryName;
+        this.countryID = countryID;
+    }
 
     public String getCountryName() {
         return countryName;
     }
 
+
     public void setCountryName(String countryName){
         this.countryName = countryName;
+    }
+
+    public int getCountryID(){
+        return countryID;
+
+    }
+
+    // will need to look at this tomorrow 12/29
+    public void setCountryID(int countryID){
+        this.countryID = countryID;
     }
 }
