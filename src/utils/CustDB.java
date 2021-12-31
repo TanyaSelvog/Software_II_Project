ed25b34d1265c9b796364eeef9e11fb569ef33d5
LabUser <tanyaselvog@gmail.com>
2021-12-31 21:28:25 +0000

    fixed SQL statement in CustDB to query correct data for Customer table

--- src/utils/CustDB.java
+++ src/utils/CustDB.java
@@ -16,26 +16,13 @@ public class CustDB {
         ObservableList<Customer> customersList = FXCollections.observableArrayList();
         try {
             //need to fix the SQL statement
-            String sqlStatement = "SELECT Customers.Customer_ID, Customers.Customer_Name, Customers.Address, " +
-                    "Customers.Postal_Code, Customers.Phone, Customers.Division_ID, "+
-                    "First_Level_Divisions.Division_ID, First_Level_Divisions.Division, Countries.Country_ID" +
-                    "FROM Customers INNER JOIN Customers ON First_Level_Divisions " +
-                    "ON Customers.Division_ID = First_Level_Divisions.Division_ID" +
-                    "INNER JOIN Countries ON First_Level_Divisions.Country_ID = Countries.Country_ID";
 
-            /** "SELECT Customers.Customer_ID, Customers.Customer_Name,
-             * Customers.Address, Customers.Postal_Code, Customers.Phone,
-             * First_Level_Divisions.Division_ID, First_Level_Divisions.Division,
-             * Country_ID.Countries, Country.Countries
-             * FROM (First_Level_Divisions
-             * INNER JOIN Customers ON First_Level_Divisions.Division_ID =Customers.Division_ID)
-             * INNER JOIN Countries ON First_Level_Divisions.Country_ID = =Countries.Country_ID)
-             *
-             * "SELECT cx.Customer_ID, cx.Customer_Name, cx.Address, cx.Postal_Code, cx.Phone, cx.Division_ID, " +
-             *        "f.Division, f.COUNTRY_ID, co.Country FROM customers as cx INNER JOIN first_level_divisions " +
-             *      "as f on cx.Division_ID = f.Division_ID INNER JOIN countries as co ON f.COUNTRY_ID = co.Country_ID");
-             *
-             */
+            String sqlStatement = "Select customer_ID, customer_Name, address, postal_code, phone, " +
+          "  customers.division_ID, first_level_divisions.division, countries.country_ID, " +
+           "  countries.country from customers, first_level_divisions, countries WHERE " +
+            "customers.division_ID = first_level_divisions.division_ID AND " +
+            "countries.country_ID = first_level_divisions.country_ID";
+
             PreparedStatement pstmt = ConnectionJDBC.openConnection().prepareStatement(sqlStatement);
 
             ResultSet result = pstmt.executeQuery();
@@ -49,10 +36,12 @@ public class CustDB {
                 String customerCountry = result.getString("Country");
                 int divisionID = result.getInt("Division_ID");
                 String customerDivision = result.getString("Division");
-
+                System.out.println(customerName + " " + customerAddress + " " + customerPostal);
                 Customer customer = new Customer(customerID, customerName, customerAddress,
-                        customerPostal, customerPhone, customerCountry, divisionID, customerDivision);
+                       customerPostal, customerPhone, customerCountry, divisionID, customerDivision);
                 customersList.add(customer);
+
+
             }
 
         } catch (SQLException exception) {
