28fd71fcf963e0cd55a2c4cd90c54fb54bf644fb
LabUser <tanyaselvog@gmail.com>
2022-01-11 04:30:09 +0000

    added method in ModifyCustomerFormController

--- src/controller/ModifyCustomerFormController.java
+++ src/controller/ModifyCustomerFormController.java
@@ -1,9 +1,14 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import model.Customer;
 
 import java.net.URL;
@@ -38,7 +43,14 @@ public class ModifyCustomerFormController implements Initializable {
     public void onSaveBtn(ActionEvent actionEvent) {
     }
 
-    public void onCancelBtn(ActionEvent actionEvent) {
+    public void onCancelBtn(ActionEvent actionEvent) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/CustomersView.fxml"));
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setTitle("All Customers");
+        Scene scene = new Scene(root, 1000, 600);
+        stage.setScene(scene);
+        stage.show();
+
     }
 
     public void onHome(ActionEvent actionEvent) {
