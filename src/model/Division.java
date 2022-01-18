9570500d84232c01040dee2343eaceb5d39ffcd4
Tanya Selvog <tanyaselvog@gmail.com>
2022-01-18 09:42:52 -0800

    added comments to Division class

--- src/model/Division.java
+++ src/model/Division.java
@@ -5,31 +5,62 @@ public class Division {
     private int divisionID;
     private int countryID;
 
-
+    /**
+     * Constructor for Division
+     * @param divisionName
+     * @param divisionID
+     * @param countryID
+     */
     public Division(String divisionName, int divisionID, int countryID){
         this.divisionName = divisionName;
         this.divisionID = divisionID;
         this.countryID = countryID;
     }
 
+    /**
+     * Getter for divisionID
+     * @return divisionID
+     */
     public int getDivisionID(){
         return divisionID;
     }
 
+    /**
+     * Setter for divisionID
+     * @param divisionID
+     */
     public void setDivisionID(int divisionID){
         this.divisionID = divisionID;
     }
+
+    /**
+     * Getter for countryID
+     * @return countryID
+     */
     public int getCountryID(){
         return countryID;
     }
 
+    /**
+     * Setter for countryID
+     * @param countryID
+     */
     public void setCountryID(int countryID){
         this.countryID = countryID;
     }
+
+    /**
+     * Getter for divisionName
+     * @return divisionName
+     */
     public String getDivisionName(){
         return divisionName;
     }
 
+    /**
+     * Setter for divisionName
+     * @param divisionName
+     */
     public void setDivisionName(String divisionName){
         this.divisionName = divisionName;
     }
