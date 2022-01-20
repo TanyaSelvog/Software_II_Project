a05397cca29b5422cc14a78f7e1df253306c5774
Tanya Selvog <tanyaselvog@gmail.com>
2022-01-20 14:01:09 -0800

    working on apptsDB method

--- src/utils/ApptsDB.java
+++ src/utils/ApptsDB.java
@@ -3,6 +3,7 @@ package utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import model.Appointments;
+import model.Customer;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -15,20 +16,39 @@ public class ApptsDB {
         ObservableList<Appointments> apptsList = FXCollections.observableArrayList();
 
         try {
-            String sqlStatement = "SELECT Title FROM Appointments";
+            String sqlStatement = "SELECT title, description, location, type, start, end, customer_ID, " +
+                    " user_ID, appointment_ID, contacts.contact_name FROM Appointments, Contacts WHERE " +
+                    "contacts.contact_ID = appointments.contact_ID";
             PreparedStatement ps = ConnectionJDBC.openConnection().prepareStatement(sqlStatement);
 
             ResultSet result = ps.executeQuery();
             while(result.next()){
+                int apptID = result.getInt("Appointment_ID");
+                String apptTitle = result.getString("Title");
+                String apptDescription = result.getString("Description");
+                String apptLocation = result.getString("Location");
+                String apptContact = result.getString("Contact");
+                String apptType = result.getString("Type");
+                System.out.println(apptTitle + " " + apptContact + " " + apptType);
+                Appointments appointments = new Appointments(apptID, apptTitle,apptDescription,apptLocation, apptContact, apptType);
                     System.out.println(result.getString("Title"));
+                    apptsList.add(appointments);
                 }
-            } catch (SQLException exception) {
+
+            /**
+             *   public Appointments(int apptID, String apptTitle, String apptDescription, String apptLocation, String apptContact,
+             *                         String apptType){
+             */
+        } catch (SQLException exception) {
                 exception.printStackTrace();
             }
             return apptsList;
             }
 
 
-
+/** SELECT appointment_ID, title, description, location, type, start, end, create_date, created_By,
+ * customer_ID, user_ID, contact_ID
+ *
+ */
 
 }
