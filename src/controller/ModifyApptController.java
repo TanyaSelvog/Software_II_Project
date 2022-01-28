bf697814066ed5769fb6edb1919e98b502bdd18f
LabUser <tanyaselvog@gmail.com>
2022-01-28 20:32:14 +0000

    made fx:id for ComboBox in ModifyAppointmentForm.fxml

--- src/controller/ModifyApptController.java
+++ src/controller/ModifyApptController.java
@@ -2,18 +2,25 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.stage.Stage;
+import utils.ContactDB;
 
-public class ModifyApptController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ModifyApptController implements Initializable {
     public Button saveBtn;
     public Button cancelBtn;
     public DatePicker startDateModAppt;
     public DatePicker endDateModAppt;
+    public ComboBox contactComboBox;
 
     public void onCancelClick(ActionEvent actionEvent) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/view/AppointmentsView.fxml"));
@@ -32,4 +39,11 @@ public class ModifyApptController {
         stage.setScene(scene);
         stage.show();
     }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        contactComboBox.setItems(ContactDB.getContactList());
+
+    }
+
 }
