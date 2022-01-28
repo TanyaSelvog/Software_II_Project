a3bc85ff07b43397de1a3eb4ab763a761ebb44c4
Tanya Selvog <tanyaselvog@gmail.com>
2022-01-28 08:32:57 -0800

    added comments for Contact class

--- src/model/Contact.java
+++ src/model/Contact.java
@@ -16,29 +16,55 @@ public class Contact {
         this.contactEmail = contactEmail;
     }
 
+    /**
+     * Getter for Contact ID
+     * @return contactID
+     */
     public int getContactID() {
         return contactID;
     }
 
+    /**
+     * Setter for Contact ID
+     * @param contactID
+     */
     public void setContactID(int contactID) {
         this.contactID = contactID;
     }
+
+    /**
+     * Getter for Contact Name
+     * @return contactName
+     */
 
     public String getContactName() {
         return contactName;
     }
 
+    /**
+     * Setter for Contact Name
+     * @param contactName
+     */
     public void setContactName(String contactName) {
         this.contactName = contactName;
     }
 
+    /**
+     * Getter for Contact Email
+     * @return contactEmail
+     */
     public String getContactEmail() {
         return contactEmail;
     }
 
+    /**
+     * Setter for Contact Email
+     * @param contactEmail
+     */
     public void setContactEmail(String contactEmail) {
         this.contactEmail = contactEmail;
     }
+
     @Override
     public String toString(){
         // return (getClass().getName() + '@' + Integer.toHexString(hashCode()));
