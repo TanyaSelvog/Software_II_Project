ab77bc1f021b05f8557b6611a1db9d2a788f030c
Tanya Selvog <tanyaselvog@gmail.com>
2022-02-01 11:35:51 -0800

    created modAppointment() in ModifyApptController

--- src/controller/ModifyApptController.java
+++ src/controller/ModifyApptController.java
@@ -11,6 +11,7 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import model.Appointments;
 import utils.ContactDB;
 
 import java.net.URL;
@@ -49,7 +50,8 @@ public class ModifyApptController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        contactComboBox.setItems(ContactDB.getContactList());
+    //    contactComboBox.setItems(ContactDB.getContactList());
+       // descriptionTF.setText(Appointments.getApptDescription());
 /**
  *  public void modCustomer(Customer customer){
  *         customerID.setText(String.valueOf(customer.getCustomerID()));
@@ -60,6 +62,15 @@ public class ModifyApptController implements Initializable {
  *         countryComboBox.setValue(customer.getCustomerCountry());
  *         divisionComboBox.setValue(customer.getCustomerDivision());
  */
+    }
+    // 2.1.22 WORKING ON THIS
+    public void modAppointment(Appointments appointment){
+        descriptionTF.setText(appointment.getApptDescription());
+        locationTF.setText(appointment.getApptLocation());
+        typeTF.setText(appointment.getApptType());
+        titleTF.setText(appointment.getApptTitle());
+        contactComboBox.setItems(ContactDB.getContactList());
+
     }
 
 }
