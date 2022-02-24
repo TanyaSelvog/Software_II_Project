c2aa9d20564a1b74acda9548a5538b7e230a3617
LabUser <tanyaselvog@gmail.com>
2022-02-24 15:00:11 -0800

    removed old comments in NewApptController

--- src/controller/NewApptController.java
+++ src/controller/NewApptController.java
@@ -62,24 +62,7 @@ public class NewApptController implements Initializable {
         startTimeCB.setItems(getTimeList());
         endTimeCB.setItems(getTimeList());
 
-   //  String textTime = timeList.format(dtf);
     }
-        //2.9 STILL Need to Fix time with dtf
-        //2.9 Displays time but want to fix display 12:00+
-
-    /**
-     * Scratch from written notes 2.23
-     * startTimeComboBox.setCellFactory((ListView<LocalTime>)-> new ListCell<LocalTime>{
-     * @Override public void updateItem(LocalTime item, boolean empty){
-     *     super.updateItem(item, empty);
-     *     if(item == null).setText("");
-     *     else setText(item.format(formatter));
-     *     }
-     *     });
-     * }
-     * }</LocalTime></LocalTime>
-     * @return
-     */
         public ObservableList<String> getTimeList() {
             ObservableList<String> timeList = FXCollections.observableArrayList();
 
