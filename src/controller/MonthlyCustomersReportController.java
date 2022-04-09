2a4def6d7382b1ff936a130a7379a620c1c69038
Tanya Selvog <tanyaselvog@gmail.com>
2022-04-09 10:51:17 -0700

    added contents to CB in MonthlyCustomerReportController

--- src/controller/MonthlyCustomersReportController.java
+++ src/controller/MonthlyCustomersReportController.java
@@ -8,12 +8,31 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
+import utils.ContactDB;
+import utils.CustDB;
+import javafx.fxml.Initializable;
 
-public class MonthlyCustomersReportController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MonthlyCustomersReportController implements Initializable{
     public Button reports;
     public Button home;
     public ComboBox monthCB;
     public ComboBox typeCB;
+    public Button generateBtn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        typeCB.getItems().addAll("Initial Meeting", "Follow-Up Consultation", "Lunch Meeting", "Closing Session");
+        //temporarily hard coding month
+        monthCB.getItems().addAll("January", "February", "March", "April", "May", "June", "July", "August","September",
+                "October", "November", "December");
+
+
+
+    }
 
     public void onReportsBtn(ActionEvent actionEvent) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/view/ReportsView.fxml"));
@@ -31,5 +50,10 @@ public class MonthlyCustomersReportController {
         Scene scene = new Scene(root, 1000, 600);
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void onGenerateRpt(ActionEvent actionEvent) {
+        //() to click to generate report based on user month & type selection
+
     }
 }
