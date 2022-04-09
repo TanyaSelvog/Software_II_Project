f4584c1899c399740eae0b1130c34a2336d33d4e
tanyaselvog@gmail.com <W0l$NF73RCSS>
2022-04-08 20:29:14 -0700

    added btn and events to new report pages

--- src/controller/ContactScheduleController.java
+++ src/controller/ContactScheduleController.java
@@ -1,4 +1,42 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+
 public class ContactScheduleController {
+    public TableView contactTable;
+    public TableColumn apptIDCol;
+    public TableColumn titleCol;
+    public TableColumn typeCol;
+    public TableColumn descCol;
+    public TableColumn startCol;
+    public TableColumn endCol;
+    public TableColumn custIDCol;
+    public Button homeBtn;
+    public Button reportsBtn;
+
+    public void onReportsBtn(ActionEvent actionEvent) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/ReportsView.fxml"));
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setTitle("Reports");
+        Scene scene = new Scene(root, 1000, 600);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void onHomeBtn(ActionEvent actionEvent) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/HomepageWindow.fxml"));
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setTitle("Scheduler Homepage");
+        Scene scene = new Scene(root, 1000, 600);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
--- src/controller/ReportsController.java
+++ src/controller/ReportsController.java
@@ -5,13 +5,35 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 //todo write three reports
 public class ReportsController {
+    public Button monthlyCust;
+    public Button contact;
+
     public void onHomeClick(ActionEvent actionEvent) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/view/HomepageWindow.fxml"));
         Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
         stage.setTitle("View Appointments");
+        Scene scene = new Scene(root, 1000, 600);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void onContactBtn(ActionEvent actionEvent) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/ContactSchedule.fxml"));
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setTitle("Contact Schedule Reports");
+        Scene scene = new Scene(root, 1000, 600);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void onMonthlyBtn(ActionEvent actionEvent) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/MonthlyCustomersReport.fxml"));
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setTitle("Monthly Customers Report");
         Scene scene = new Scene(root, 1000, 600);
         stage.setScene(scene);
         stage.show();
