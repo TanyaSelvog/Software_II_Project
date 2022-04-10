60aa98dd190e7020026f3b37d8813e59bba6ed1a
tanyaselvog@gmail.com <W0l$NF73RCSS>
2022-04-09 18:42:05 -0700

    created OL<Month> in MonthlyCustomersReportController

--- src/controller/MonthlyCustomersReportController.java
+++ src/controller/MonthlyCustomersReportController.java
@@ -22,7 +22,9 @@ import java.util.ResourceBundle;
 public class MonthlyCustomersReportController implements Initializable{
     public Button reports;
     public Button home;
-    public ComboBox monthCB;
+
+
+    public ComboBox<Month> monthCB;
     public ComboBox typeCB;
     public Button generateBtn;
     public TextArea reportsTA;
@@ -31,19 +33,17 @@ public class MonthlyCustomersReportController implements Initializable{
     public TableColumn typeCol;
     public TableColumn apptCol;
     private int month;
-
-    private static ObservableList<String> months = FXCollections.observableArrayList("January", "February", "March", "April", "May", "June",
+    private static ObservableList<String> m = FXCollections.observableArrayList("January", "February", "March", "April", "May", "June",
             "July", "August", "September", "October", "November", "December");
 
+    private static ObservableList<Month> months = FXCollections.observableArrayList(Month.JANUARY, Month.FEBRUARY, Month.MARCH,
+    Month.APRIL, Month.MAY, Month.JUNE, Month.JULY, Month.AUGUST, Month.SEPTEMBER, Month.OCTOBER, Month.NOVEMBER, Month.DECEMBER);
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
         typeCB.getItems().addAll("Initial Meeting", "Follow-Up Consultation", "Lunch Meeting", "Closing Session");
-        //temporarily hard coding month
-        monthCB.setItems(months);
 
-
-
+           monthCB.setItems(months);
     }
 
     public void onReportsBtn(ActionEvent actionEvent) throws Exception{
@@ -66,19 +66,22 @@ public class MonthlyCustomersReportController implements Initializable{
     }
 
     public void onGenerateRpt(ActionEvent actionEvent) {
-        ObservableList<Appointments> monthTypeList = FXCollections.observableArrayList();
+   //     ObservableList<Appointments> monthTypeList = FXCollections.observableArrayList();
         //() to click to generate report based on user month & type selection
-        month = monthCB.getSelectionModel().getSelectedIndex()+1;
 
-        if (ApptsDB.getMonthType(month).isEmpty()) {
+          //  month = monthCB.getSelectionModel().getSelectedIndex()+1;
+
+   /**     if (ApptsDB.getMonthType(month).isEmpty()) {
 
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setTitle("No Appointments");
             alert.setContentText("No appointments in this month.");
             alert.showAndWait();
-
-        }
+*/
+      //  }
        // reportsTA.setText(ApptsDB.getApptByMonthAndType(month));
-        System.out.println(ApptsDB.getMonthType(month));
+  //      System.out.println(ApptsDB.getMonthType(month));
     }
+
+
 }
