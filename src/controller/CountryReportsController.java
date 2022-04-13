dc2f41ee143e79b610cf46cd31adfbeb79588948
Tanya Selvog <tanyaselvog@gmail.com>
2022-04-13 10:29:40 -0700

    updated fx id tags in CountryReports FXML

--- src/controller/CountryReportsController.java
+++ src/controller/CountryReportsController.java
@@ -1,4 +1,14 @@
 package controller;
 
+import javafx.event.ActionEvent;
+
 public class CountryReportsController {
+    public void onHomeBtn(ActionEvent actionEvent) {
+    }
+
+    public void onGenerateRpt(ActionEvent actionEvent) {
+    }
+
+    public void onReportsBtn(ActionEvent actionEvent) {
+    }
 }
